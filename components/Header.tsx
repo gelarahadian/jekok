1cdc3438ca23c0dbf4d582c3370f9cedf02d5c2e
gelarahadian <gelarahadian38@gmail.com>
2024-09-07 13:46:19 +0700

    fix: resolve logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="py-3 px-4 border-header-bottom border-b">
       <div className="flex justify-between">
         <div>
-          <Image src={"/assets/logo.png"} alt="logo" width={38} height={38} />
+          <Image src={"/assets/Logo.png"} alt="logo" width={38} height={38} />
         </div>
         <div className="flex space-x-3">
           <div className="p-1 border rounded-xl border-button-secondary">
